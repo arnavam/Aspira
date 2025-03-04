c0d0068b3ffbed18c22bb36526971ce3eae59236
arnavam <arnavjagadeesh09@gmail.com>
2025-03-04 21:12:00 +0530

    converted aspira to fn

--- aspira.py
+++ aspira.py
@@ -31,123 +31,123 @@ timer=4
 question ="what is machine learning"
 #or question= "what subjects do you like?"
 #convert(question)
+kw={}
 
-while(timer!=0):
-    timer-=1
-    text = '''Machine learning teaches computers to recognize patterns and make decisions automatically using data and algorithms.
+def aspira():
 
-It can be broadly categorized into three types:
+        text = '''Machine learning teaches computers to recognize patterns and make decisions automatically using data and algorithms.
 
-Supervised Learning: Trains models on labeled data to predict or classify new, unseen data.
-Unsupervised Learning: Finds patterns or groups in unlabeled data, like clustering or dimensionality reduction.
-Reinforcement Learning: Learns through trial and error to maximize rewards, ideal for decision-making tasks.
-In addition these categories, there are also Semi-Supervised Learning and Self-Supervised Learning. 
+    It can be broadly categorized into three types:
 
-Semi-Supervised Learning uses a mix of labeled and unlabeled data, making it helpful when labeling data is costly or time-consuming. 
-Self-Supervised Learning creates its own labels from raw data, allowing it to learn patterns without needing labeled examples. 
-Machine Learning Pipeline
-Machine learning is fundamentally built upon data, which serves as the foundation for training and testing models. Data consists of inputs (features) and outputs (labels). A model learns patterns during training and is tested on unseen data to evaluate its performance and generalization. In order to make predictions, there are essential steps through which data passes in order to produce a machine learning model that can make predictions.'''
-    # text='I like machine learning'
-    while(text == "Not found"):
-        text=speech() 
-        if text =="stop":
-            break       
-    keywords=extract(text)
-    #if len(list(keywords.values)) < 2 and all(x < 8 for x in list(keywords.values)):
-    #   convert('ok')
-    kw={}
-    for  key , score in keywords.items():
-        sm=compute_similarity('machine learning',key)
-        kw[key] = (score,sm)
-        #value = my_dict.pop('b')
+    Supervised Learning: Trains models on labeled data to predict or classify new, unseen data.
+    Unsupervised Learning: Finds patterns or groups in unlabeled data, like clustering or dimensionality reduction.
+    Reinforcement Learning: Learns through trial and error to maximize rewards, ideal for decision-making tasks.
+    In addition these categories, there are also Semi-Supervised Learning and Self-Supervised Learning. 
 
-        # print(f"{sm:.2f}:{score:.2f}:{key}")
-    #kw={name:kw.get(name,0)+keywords[name] for name in set(kw)| set(keywords)}
-    # sorted_scores = dict(sorted(kw.items(), key=lambda x: x[1], reverse=True))
-    # sorted_scores= dict(sorted(kw.items(), key=lambda x: (x[1][0], x[1][1]), reverse=True))
+    Semi-Supervised Learning uses a mix of labeled and unlabeled data, making it helpful when labeling data is costly or time-consuming. 
+    Self-Supervised Learning creates its own labels from raw data, allowing it to learn patterns without needing labeled examples. 
+    Machine Learning Pipeline
+    Machine learning is fundamentally built upon data, which serves as the foundation for training and testing models. Data consists of inputs (features) and outputs (labels). A model learns patterns during training and is tested on unseen data to evaluate its performance and generalization. In order to make predictions, there are essential steps through which data passes in order to produce a machine learning model that can make predictions.'''
+        text='i very much like machine learning'
+        #text='Not found'
+        while(text == "Not found"):
+            text=speech() 
+            if text =="stop":
+                break       
+        keywords=extract(text)
+        #if len(list(keywords.values)) < 2 and all(x < 8 for x in list(keywords.values)):
+        #   convert('ok')
+        
+        for  key , score in keywords.items():
+            sm=compute_similarity('machine learning',key)
+            kw[key] = (score,sm)
+            #value = my_dict.pop('b')
 
-    # Calculate the thresholds
-    first_elements = [v[0] for v in kw.values()]
-    second_elements = [v[1] for v in kw.values()]
+            # print(f"{sm:.2f}:{score:.2f}:{key}")
+        #kw={name:kw.get(name,0)+keywords[name] for name in set(kw)| set(keywords)}
+        # sorted_scores = dict(sorted(kw.items(), key=lambda x: x[1], reverse=True))
+        # sorted_scores= dict(sorted(kw.items(), key=lambda x: (x[1][0], x[1][1]), reverse=True))
 
-    first_threshold = sorted(first_elements)[len(first_elements) // 2]  # Middle of first elements
-    second_threshold = sorted(second_elements)[len(second_elements) // 2]  # Middle of second elements
+        # Calculate the thresholds
+        first_elements = [v[0] for v in kw.values()]
+        second_elements = [v[1] for v in kw.values()]
 
-    # Print the thresholds for clarity
-    print(f"First threshold: {first_threshold}")
-    print(f"Second threshold: {second_threshold}")
+        first_threshold = sorted(first_elements)[len(first_elements) // 2]  # Middle of first elements
+        second_threshold = sorted(second_elements)[len(second_elements) // 2]  # Middle of second elements
 
-    # Sorting based on custom conditions:
-    sorted_scores = dict(sorted(kw.items(), key=lambda x: (
-        # Check if both values are high (greater than their respective thresholds)
-    not (x[1][0] <= first_threshold and x[1][1] <= second_threshold), 
-        x[1][1],  # Then sort by the first element
-        x[1][0]  # Then sort by the second element
-    ), reverse=True))
+        # Print the thresholds for clarity
+        print(f"First threshold: {first_threshold}")
+        print(f"Second threshold: {second_threshold}")
+
+        # Sorting based on custom conditions:
+        sorted_scores = dict(sorted(kw.items(), key=lambda x: (
+            # Check if both values are high (greater than their respective thresholds)
+        not (x[1][0] <= first_threshold and x[1][1] <= second_threshold), 
+            x[1][1],  # Then sort by the first element
+            x[1][0]  # Then sort by the second element
+        ), reverse=True))
 
 
-    for  key , score in sorted_scores.items():
-        f_score = (f"{score[0]:.2f}", f"{score[1]:.2f}")
+        for  key , score in sorted_scores.items():
+            f_score = (f"{score[0]:.2f}", f"{score[1]:.2f}")
 
-        print(f"{f_score}:{key}")
+            print(f"{f_score}:{key}")
+        kw=sorted_scores
+        count1=3
+        for  key , score in keywords.items() :
+            if score >= 2 and len(key.split(' ')) < 4   :
+                count1 -=1
+                if (count1==0):
+                    break
+                links =search(key,no=2)
+                for  no , link in enumerate(links,1):
+                    text =Parse(link)
+                    if text == 'skip' or text is None:
+                        continue
+                    # with open(f'file/{no}.txt','w') as file:
+                    #     file.writelines(text)
 
-    count1=3
-    for  key , score in keywords.items() :
-        if score >= 2 and len(key.split(' ')) < 4   :
-            count1 -=1
-            if (count1==0):
-                break
-            links =search(key,no=2)
-            for  no , link in enumerate(links,1):
-                text =Parse(link)
-                if text == 'skip' or text is None:
-                    continue
-                # with open(f'file/{no}.txt','w') as file:
-                #     file.writelines(text)
-
-                chunks = split_text_into_chunks(text, max_tokens=200)
-                
-                final_summary = "{}{}{}".join(chunks)
-                # with open('file/summary.txt','w') as file:
-                #     file.writelines(final_summary)
-                print(len(chunks))
-                count=0
+                    chunks = split_text_into_chunks(text, max_tokens=200)
                     
-                for i in range(min(len(chunks),5)):
-                    l=chatbot(chunks[i])
-                    while(count!=3):
-                        #convert(l)
-                        count+=1
-                    score = compute_similarity(question, l)
-                    qa[l]=score
+                    final_summary = "{}{}{}".join(chunks)
+                    # with open('file/summary.txt','w') as file:
+                    #     file.writelines(final_summary)
+                    print(len(chunks))
+                    count=0
+                        
+                    for i in range(min(len(chunks),5)):
+                        l=chatbot(chunks[i])
+                        while(count!=3):
+                            #convert(l)
+                            count+=1
+                        score = compute_similarity(question, l)
+                        qa[l]=score
 
 
-    sorted_qa = dict(sorted(qa.items(), key=lambda x: x[1], reverse=True))
-    values = list(sorted_qa.values())
-    # mean_value = sum(values) / len(values)
-    centre_value=np.mean(values)
-    closest_key = min(sorted_qa, key=lambda k: abs(sorted_qa[k] - centre_value))
-    print('Selected Question')
-    print('-'*50)
-    print(f"{sorted_qa[closest_key]:.2f}:{closest_key}")
-    question=closest_key
-    print(time.perf_counter()-start_time)
+        sorted_qa = dict(sorted(qa.items(), key=lambda x: x[1], reverse=True))
+        values = list(sorted_qa.values())
+        # mean_value = sum(values) / len(values)
+        centre_value=np.mean(values)
+        closest_key = min(sorted_qa, key=lambda k: abs(sorted_qa[k] - centre_value))
+        print('Selected Question')
+        print('-'*50)
+        print(f"{sorted_qa[closest_key]:.2f}:{closest_key}")
+        question=closest_key
+        print(time.perf_counter()-start_time)
 
-    convert(question)
-    print(time.perf_counter()-start_time)
+        convert(question)
+        print(time.perf_counter()-start_time)
 
+        yield 'start' 
+
+
+
+
+
+                
 
 
 
 
 
             
-
-
-
-
-
-        
-
-
-    
